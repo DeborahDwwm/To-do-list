3c76867998d30ca2c26c21af27b9be45aa78bf6c
DFDWWM <frontin.deborah@gmail.com>
2024-07-25 08:44:59 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+//recupérer la saisie de l'utilisateur dans un console.log//
+
+const taskInput = document.querySelector("button");
+
+taskInput.addEventListener("click", (button) => {
+    let recupTask = document.getElementById("task");
+    let task = recupTask.value
+    
+    let listTask = document.getElementById("ulPerso");
+    let li = document.createElement('li');
+    li.textContent = task;
+    listTask.appendChild(li);
+    console.log(listTask);
+    
+    const boutonCheck = document.createElement("img");
+    boutonCheck.src = "square-check-solid.svg";
+    li.appendChild(boutonCheck);
+    boutonCheck.addEventListener("click",() => {
+    li.style.textDecoration ="line-through";
+})
+    const boutonCross = document.createElement("img");
+    boutonCross.src = "square-xmark-solid.svg";
+    li.appendChild(boutonCross);
+    boutonCross.addEventListener("click",() => {
+        li.remove()
+})
+})
+
+// const boutonCheck = document.createElement("button");
+//     boutonCheck.textContent="réalisée";
+//     li.appendChild(boutonCheck);
+//     boutonCheck.addEventListener("click",() => {
+//     li.style.textDecoration ="line-through";
+
+
+
+
+
+
+
+//valider les tâches avec un bouton check//
+// let liBouton = document.querySelector("li")
+//     let boutonCheck = document.createElement("img");
+//     boutonCheck.src = "square-check-solid.svg"
+//     li.appendChild(liBouton);
+
+ 
+//lui faire rayer la tâche//
+
+//supprimer les tâches avec un boutoncross//
+
+// let liBouton2 = document.querySelector("li")
+// let boutonCross = document.createElement("img");
+// boutonCross.src = "square-xmark-solid.svg"
+// li.appendChild(liBouton2);
+
